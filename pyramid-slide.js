2b9783a3a799f6d732a74c6188d573bb3a373e2c
James Powell <surrealworldssurroundme@gmail.com>
2017-03-06 19:20:23 -0600

    done with functionality except symbol choice only changes after adjusting slider, need it to change when selected using onchange property, css needs finished.

---
+++ pyramid-slide.js
@@ -0,0 +1,53 @@
+
+/**
+ * determineHeightAndThenDrawPyramid
+ * Determines the current value from the heightStr variable
+ */
+function determineHeightAndThenDrawPyramid() {
+
+    var heightStr = document.getElementById("height").value;
+    // here we convert the string to an int
+    height = parseInt(heightStr);
+
+    drawPyramid(height)
+}
+
+
+
+/**
+ * drawPyramid
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+
+function drawPyramid(height) {
+    
+     // before drawing, clear the old content
+     document.getElementById("pyramid").innerHTML = " ";
+     // for each row....
+     var symbol = document.getElementById("symbol").value
+     for (var row = 0; row < height; row++) {
+
+         // figure out number of bricks and spaces
+         var numBricks = row + 2;
+         var numSpaces = height - row - 1;
+
+         // build up a string for this row
+         var rowStr = "";
+         for (var i = 0; i < numSpaces; i++) {
+             rowStr += ".";
+         }
+         for (var i = 0; i < numBricks; i++) {
+             rowStr += symbol;
+         }
+
+        // create a text element with the string of characters
+        textElem = document.createTextNode(rowStr);
+
+        // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
